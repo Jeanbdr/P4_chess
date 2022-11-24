0465c415739c059d840306d0046abf503b114359
Jb <drts.alexis@gmail.com>
2022-11-24 13:51:52 +0100

    major update tournament views

--- views/tournament.py
+++ views/tournament.py
@@ -1,73 +1,84 @@
-from model.tournament import ROUND_NUMBERS, PARTICIPANTS, TODAY
-from db import db_player, Check
+from views.view import View
+from controller.timestamp import get_timestamp
+from controller.database import load_db
 
 
-class CreateTournament:  # NOM A CHANGER
-    """Demande à l'utilisateur les informations nécessaires pour la création d'un tournoi"""
+class CreateTournament(View):
+    def display_menu(self):
 
-    def input_tournament(self):
+        # date = get_timestamp()
+        # print(date + " : Nouveau tournoi")
 
-        name = input("Saisissez le nom du tournoi: ")
-        place = input("Saisissez le lieu du tournoi: ")
-        while True:
-            try:
-                time_control = int(
-                    input(
-                        f"Choisissez le type de tournoi : \n"
-                        f"0 >> Bullet \n"
-                        f"1 >> Coup rapide \n"
-                        f"2 >> Blitz \n"
-                        f">>> "
-                    )
-                )
-            except ValueError:
-                print("Choix non valable merci de choisir entre 0, 1 ou 2")
-            if time_control not in (0, 1, 2):
-                print(
-                    "Le type de tournoi n'est pas valide merci de choisir entre 0, 1 ou 2"
-                )
-            elif time_control == 0:
-                time_control = "Bullet"
-                break
-            elif time_control == 1:
-                time_control = "Coup rapide"
-                break
-            elif time_control == 2:
-                time_control = "Blitz"
-                break
-            else:
-                break
+        name = input("Nom du tournoi: ")
 
-        while True:
-            try:
-                participants = int(
-                    input("Saisissez le nombre de joueurs dans le tournoi: ")
-                )
-            except ValueError:
-                print("Erreur : Merci de saisir une valeur numérique")
+        place = self.get_user_entry(
+            msg_display="Lieu: ",
+            msg_error="Veuillez entrer un lieu valide",
+            value_type="string",
+        )
 
-            else:
-                if participants < PARTICIPANTS:
-                    print(
-                        "Nombre de participants trop faible le tournoi ne peut être crée. (8 minimum)"
-                    )
-                    continue
-                break
+        user_selection_time_control = self.get_user_entry(
+            msg_display="Contrôle de temps:\n0 - Bullet\n1 - Blitz\n2 - Coup Rapide\n>>> ",
+            msg_error="Veuillez entrer 0, 1 ou 2.",
+            value_type="selection",
+            assertions=["0", "1", "2"],
+        )
+        if user_selection_time_control == "0":
+            time_control = "Bullet"
+        elif user_selection_time_control == "1":
+            time_control = "Blitz"
+        else:
+            time_control = "Coup Rapide"
 
-        description = input("Saisissez une description du tournoi (optionnelle): ")
+        nb_players = self.get_user_entry(
+            msg_display="Nombre de joueurs: ",
+            msg_error="Veuillez entrer un nombre entier supérieur ou égal à 2.",
+            value_type="num_superior",
+            default_value=2,
+        )
+
+        nb_rounds = self.get_user_entry(
+            msg_display="Nombre de tours (4 par défaut): ",
+            msg_error="Veuillez entrer 4 ou plus.",
+            value_type="num_superior",
+            default_value=4,
+        )
+        desc = input("Description du tournoi: ")
 
         return {
             "name": name,
             "place": place,
+            "date": get_timestamp(),
             "time_control": time_control,
-            "participants": participants,
-            "today": TODAY,
-            "round_number": ROUND_NUMBERS,
-            "description": description,
+            "nb_players": nb_players,
+            "nb_rounds": nb_rounds,
+            "desc": desc,
         }
 
-    def print_leaderboard(self, players):
-        print()
-        for player in sorted(players, key=lambda player: player.score, reverse=True):
-            print(f"{player} avec un score de {player.score} ")
-        print()
+
+class LoadTournament(View):
+    def display_menu(self):
+
+        all_tournaments = load_db("tournaments")
+        if all_tournaments:
+
+            builded_selection = self.build_selection(
+                iterable=all_tournaments,
+                display_msg="Choisir un tournoi:\n",
+                assertions=[],
+            )
+
+            user_input = int(
+                self.get_user_entry(
+                    msg_display=builded_selection["msg"] + "\n> ",
+                    msg_error="Veuillez entrer un nombre entier.",
+                    value_type="selection",
+                    assertions=builded_selection["assertions"],
+                )
+            )
+            serialized_loaded_tournament = all_tournaments[user_input - 1]
+
+            return serialized_loaded_tournament
+
+        else:
+            return False
