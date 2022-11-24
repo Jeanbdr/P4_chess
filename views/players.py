4e6dbaadd10397449dcf0e742c68e9b5aea150c5
Jb <drts.alexis@gmail.com>
2022-11-24 13:49:04 +0100

    major update view player

--- views/players.py
+++ views/players.py
@@ -1,107 +1,74 @@
-import datetime, re
-from db import Check, db_player, where
+from views.view import View
+from controller.database import load_db
 
 
-class NegativeValue(Exception):
-    """Raised when elo is negative"""
+class CreatePlayer(View):
+    def display_menu(self):
 
-    pass
+        first_name = input("Prénom du joueur: ")
 
+        name = input("Nom du joueur: ")
 
-class IdExist(Exception):
-    """Raised when ID already exist"""
+        birthdate = self.get_user_entry(
+            msg_display="Date de naissance (format DD/MM/YYYY): ",
+            msg_error="Veuillez entrer une date au format valide: DD/MM/YYYY",
+            value_type="date",
+        )
 
-    pass
+        gender = self.get_user_entry(
+            msg_display="Sexe (M ou F): ",
+            msg_error="Veuillez entrer M ou F",
+            value_type="selection",
+            assertions=["M", "m", "F", "f"],
+        ).upper()
 
+        ranking = self.get_user_entry(
+            msg_display="Elo du joueur: ",
+            msg_error="Veuillez entrer une valeur numérique valide.",
+            value_type="numeric",
+        )
 
-class PlayerView:
-    """Affiche les vues"""
-
-    def input_player(self):  # VALIDE
-        """Demande à l'utilisateur les informations permettant la création d'un joueur"""
-
-        while True:
-            first_name = input("Prénom du joueur: ")
-            last_name = input("Nom du joueur: ")
-            if re.search("[0-9]", first_name):
-                print("Prénom invalide")
-            elif re.search("[0-9]", last_name):
-                print("Nom invalide")
-            else:
-                break
-
-        while True:
-            birthdate = input("Date de naissance du joueur (JJ/MM/AAAA): ")
-            try:
-                datetime.datetime.strptime(birthdate, "%d/%m/%Y")
-            except ValueError:
-                print("Date non valide merci d'utiliser le format JJ/MM/AAAA")
-            else:
-                break
-
-        while True:
-            gender = input("Genre du joueur (M ou F): ").upper()
-            if gender not in ("M", "F"):
-                print("Genre non valide merci de saisir un genre valide")
-            else:
-                break
-
-        while True:
-            try:
-                ranking = int(input("Elo du joueur (valeur numérique positive): "))
-                if ranking < 0:
-                    raise NegativeValue("Veuillez saisir un elo positif")
-            except ValueError:
-                print("L'elo du joueur n'est pas valide merci de saisir un elo valide ")
-            else:
-                break
-
-        while True:
-            player_id = int(input("Id choisis par le joueur (exemple : 1234): "))
-            already_taken = db_player.contains(Check.player_id == player_id)
-            if already_taken == True:
-                print("L'id est déjà choisis veuillez en choisir un autre")
-            else:
-                break
-
-        print(f"{first_name} {last_name} a été créé")
+        print(f"{first_name} {name} a été créé.")
 
         return {
             "first_name": first_name,
-            "last_name": last_name,
+            "name": name,
             "birthdate": birthdate,
             "gender": gender,
-            "ranking": ranking,
-            "player_id": player_id,
             "total_score": 0,
+            "ranking": ranking,
         }
 
-    def change_elo(self):
-        while True:
-            try:
-                new_elo = int(input(f"Nouvel ELO :\n" f">>>"))
-            except ValueError:
-                print("Elo non valide merci de saisir un elo valide")
-            else:
-                break
-        return new_elo
 
-    def search_player(self):
-        while True:
-            try:
-                player_id = int(input("ID du joueur recherché \n >>>"))
-            except ValueError:
-                print("ID non valable merci de saisir un ID existant")
-            else:
-                break
-        return player_id
+class LoadPlayer(View):
+    def display_menu(self, nb_players_to_load):
 
-
-"""
-class LoadPlayer:
-
-    def display_menu(self, nb_player):
         all_players = load_db("players")
-        ser_loaded_player = []
-        for i in range(nb_player):
-"""
+        serialized_loaded_players = []
+        for i in range(nb_players_to_load):
+            print(f"Plus que {str(nb_players_to_load - i)} joueurs à charger.")
+            display_msg = "Choisir un joueur:\n"
+
+            assertions = []
+            for i, player in enumerate(all_players):
+                display_msg = (
+                    display_msg
+                    + f"{str(i+1)} - {player['first_name']} {player['name']}\n"
+                )
+                assertions.append(str(i + 1))
+
+            user_input = int(
+                self.get_user_entry(
+                    msg_display=display_msg,
+                    msg_error="Veuillez entrer un nombre entier.",
+                    value_type="selection",
+                    assertions=assertions,
+                )
+            )
+            if all_players[user_input - 1] not in serialized_loaded_players:
+                serialized_loaded_players.append(all_players[user_input - 1])
+            else:
+                print("Joueur déjà chargé veuillez choisir un autre joueur.")
+                nb_players_to_load += 1
+
+        return serialized_loaded_players
